4e3c3ba68e8b21dee9152f51391dd06b813773a3
MoatazEssam <motaz.mostafa99@gmail.com>
2022-12-16 01:52:11 +0200

    adding aboutus landing video and heading

---
+++ pizzahouse/src/Components/Aboutus/Aboutus.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from '../HomePage/Header/Header';
+import Land from './land/land';
+
+class Aboutus extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Land />
+        
+      </div>
+    )
+  }
+}
+
+export default Aboutus
---
+++ pizzahouse/src/Components/Aboutus/land/land.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './land.css'
+import k1 from '../imgs/Pizza Cheese GIF by Little Caesars - Find & Share on GIPHY.gif'
+
+export class land extends Component {
+  render() {
+    return (
+      <div>
+        <div className='land'>
+   <img src={k1} alt=''></img>
+   <h1>About us</h1>
+   </div>
+        
+      </div>
+    )
+  }
+}
+
+export default land
