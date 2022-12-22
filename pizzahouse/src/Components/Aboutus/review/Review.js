d0e48337affc3043b94b6209bc98ec634df3135c
MoatazEssam <motaz.mostafa99@gmail.com>
2022-12-22 04:00:13 +0200

    adding the teams rule and adding  reviews

---
+++ pizzahouse/src/Components/Aboutus/review/Review.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Review.css'
+import '../../general.css'
+
+ class land extends Component {
+  render() {
+    return (
+      <div >
+        <div className='rev'>
+     <h2>Customers Reviews</h2>
+   </div>
+        
+      </div>
+    )
+  }
+}
+
+export default land
