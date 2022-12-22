4ed68081a0c4e7ca0a03c38f44273688bda92075
M0hamedAbdallah <mohaabdallah2002287@gmail.com>
2022-12-22 13:03:04 -0800

    change in login in function

--- pizzahouse/src/Components/RegisterAndLogIn/Login.js
+++ pizzahouse/src/Components/RegisterAndLogIn/Login.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React , {Component} from 'react'
 
-function Login() {
-  return (
-    <div>
+class Login extends Component{
+  render(){
+    return (
+      <div>
         
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default Login
