2110cb8ded76a7f000a64f07c040bab12abadec5
Belal <asama4278@gmail.com>
2022-12-23 15:06:19 +0200

    Desserts menu

--- pizzahouse/src/Components/MenuPage/Desserts.js
+++ pizzahouse/src/Components/MenuPage/Desserts.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import '../general.css'
+import Card from '../pruducts/Card'
+import img1 from '../imgs/Desserts/Cheesecake.png'
+import img2 from '../imgs/Desserts/Crème caramel.png'
+import img3 from '../imgs/Desserts/Gelatin pudding.png'
+import img4 from '../imgs/Desserts/Kunafa.png'
+import img5 from '../imgs/Desserts/Molten chocolate cake.png'
+import img6 from '../imgs/Desserts/Chocolate ice cream.png'
+import img7 from '../imgs/Desserts/ice cream cup with strawberries.png'
+import img8 from '../imgs/Desserts/waffles with strawberries.png'
 import '../pruducts/card.css'
 function Desserts() {
   return (
     <div className='cardpage'>
       <div className="container">
+        <Card img={img1} name="/Cheesecake" price="18$"/>
+        <Card img={img2} name="Crème caramel" price="18$"/>
+        <Card img={img3} name="Gelatin pudding" price="18$"/>
+        <Card img={img4} name="Kunafa with Nuts" price="18$"/>
+        <Card img={img5} name="Molten chocolate cake" price="18$"/>
+        <Card img={img6} name="Chocolate ice cream" price="18$"/>
+        <Card img={img7} name="ice cream cup with strawberries" price="18$"/>
+        <Card img={img8} name="waffles with strawberries" price="18$"/>
       </div>
     </div>
   )
