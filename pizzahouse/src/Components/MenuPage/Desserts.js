219af9af1d7ab21c225b5421274bc695e6cf218b
Belal <asama4278@gmail.com>
2022-12-23 15:03:39 +0200

    Seafood menu

--- pizzahouse/src/Components/MenuPage/Desserts.js
+++ pizzahouse/src/Components/MenuPage/Desserts.js
@@ -1,33 +1,10 @@
 import React from 'react'
 import '../general.css'
-import Card from '../pruducts/Card'
-import img1 from '../imgs/Products/product-1-161x162.png'
-import img2 from '../imgs/Products/product-2-161x162.png'
-import img3 from '../imgs/Products/product-3-161x162.png'
-import img4 from '../imgs/Products/product-4-161x162.png'
 import '../pruducts/card.css'
 function Desserts() {
   return (
     <div className='cardpage'>
       <div className="container">
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
-        <Card img={img1} name="Arabic Fattoush" price="18$"/>
       </div>
     </div>
   )
