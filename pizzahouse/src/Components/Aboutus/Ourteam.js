60afd6249fb6d2fc62f2a6576b67c12eac8de70e
MoatazEssam <motaz.mostafa99@gmail.com>
2022-12-29 04:48:55 +0200

    editing about us

--- pizzahouse/src/Components/Aboutus/Ourteam.js
+++ pizzahouse/src/Components/Aboutus/Ourteam.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Ourteam.css'
-import '../general.css'
+
 function Ourteam(props) {
   return (
     
     <div className='ourteam'>
-      <img src={props.img} alt="" />
-   <div className="inf">
-   <h3>{props.name}</h3>
-      <h4>{props.rule}</h4>
-   </div>
+        <div className='faces'>
+            <img src={props.img} alt=''  width="219" height="300"/>
+            <h3>{props.name}</h3>
+            <p>{props.rule}</p>
+            <div class="load-wrapp">
+      <div class="load-10">
+        <div class="bar"></div>
+      </div>
+    </div>
   </div>
-
+</div>
   )
 }
 
